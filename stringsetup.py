9061e518dc9c31d4b5df636580ad403d814eb877
Abhishek singh <84198635+xAbhish3k@users.noreply.github.com>
2022-07-12 14:40:40 +0530

    Update stringsetup.py

--- stringsetup.py
+++ stringsetup.py
@@ -18,14 +18,14 @@ if okvai == "69":
         API_HASH = input("\nEnter API HASH here: ")
         with TelegramClient(StringSession(), APP_ID, API_HASH) as hellbot:
             print("\nYour Waruserbot Session Is sent in your Telegram Saved Messages.")
-            hellbot.send_message("me", f"#WARUSERBOT #WARUSERBOT_SESSION \n\n`{hellbot.session.save()}`")
+            warbot.send_message("me", f"#WARUSERBOT #WARUSERBOT_SESSION \n\n`{warbot.session.save()}`")
     elif library == "2":
         print("Pyrogram Session for Music Bot")
         APP_ID = int(input("\nEnter APP ID here: "))
         API_HASH = input("\nEnter API HASH here: ")
         with Client(':memory:', api_id=APP_ID, api_hash=API_HASH) as hellbot:
             print("\nYour WarBot Session Is sent in your Telegram Saved Messages.")
-            hellbot.send_message("me", f"#WARUSERBOT_MUSIC #WARUSERBOT_SESSION\n\n`{hellbot.export_session_string()}`")
+            warbot.send_message("me", f"#WARUSERBOT_MUSIC #WARUSERBOT_SESSION\n\n`{warbot.export_session_string()}`")
     else:
         print("Please Enter 1 or 2 only.")
 else:
