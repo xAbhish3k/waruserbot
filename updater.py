4f131a4700720e06b73b6c5d44b2e4a5c11e622e
Kunal Gaikwad <129763781+AnimeCampus@users.noreply.github.com>
2024-01-13 04:44:16 +0530

    Update updater.py

--- updater.py
+++ updater.py
@@ -2,20 +2,16 @@ import asyncio
 import difflib
 import shlex
 import sys
-
 from typing import Tuple
 
-
-async def lines_differnce(file1, file2):
+async def lines_difference(file1, file2):
     with open(file1) as f1:
         lines1 = f1.readlines()
         lines1 = [line.rstrip("\n") for line in lines1]
     with open(file2) as f2:
         lines2 = f2.readlines()
         lines2 = [line.rstrip("\n") for line in lines2]
-    diff = difflib.unified_diff(
-        lines1, lines2, fromfile=file1, tofile=file2, lineterm="", n=0
-    )
+    diff = difflib.unified_diff(lines1, lines2, fromfile=file1, tofile=file2, lineterm="", n=0)
     lines = list(diff)[2:]
     added = [line[1:] for line in lines if line[0] == "+"]
     removed = [line[1:] for line in lines if line[0] == "-"]
@@ -23,8 +19,7 @@ async def lines_differnce(file1, file2):
     removedt = [i for i in removed if i not in added]
     return additions, removedt
 
-
-async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
+async def run_command(cmd: str) -> Tuple[str, str, int, int]:
     args = shlex.split(cmd)
     process = await asyncio.create_subprocess_exec(
         *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
@@ -37,18 +32,18 @@ async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
         process.pid,
     )
 
-
-async def update_requirements(main , test):
-    a, r = await lines_differnce(main, test)
+async def update_requirements(main, test):
+    additions, removed = await lines_difference(main, test)
     try:
-        for i in a:
-            await runcmd(f"pip install {i}")
-            print(f">> Installed Requirement: {i}")
+        for requirement in additions:
+            await run_command(f"pip install {requirement}")
+            print(f">> Installed Requirement: {requirement}")
     except Exception as e:
-        print(f"Error installing requirments {str(e)}")
+        print(f"Error installing requirements: {str(e)}")
 
-
-# loop = asyncio.get_running_loop()
-# loop.run_until_complete(update_requirements(sys.argv[1] , sys.argv[2]))
-# loop.close()
-asyncio.run(update_requirements(sys.argv[1] , sys.argv[2]))
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <main_requirements.txt> <test_requirements.txt>")
+        sys.exit(1)
+    
+    asyncio.run(update_requirements(sys.argv[1], sys.argv[2]))
